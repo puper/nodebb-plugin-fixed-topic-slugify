5d614df828a52919095e0283b71a8edd9556cb6b
Pu Pu <master@131411.xyz>
2024-10-07 14:49:43 +0800

    up

--- plugin.js
+++ plugin.js
@@ -1,25 +1,13 @@
-/**
- * Created by Nicolas on 6/19/15.
- */
-(function (Plugin) {
-    'use strict';
-    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
-    Plugin.hooks = {
-        filters: {
-            topicCreate: function (topicData) {
-                topicData.topic.slug = `${topicData.topic.tid}/topic`;
-                console.log(topicData.topic.slug);
-                //callback(null, topicData);
-                return topicData;
-            },
 
-            topicEdit: function (topicData) {
-                topicData.slug = `${topicData.topic.tid}/topic`;
-                //callback(null, topicData);
-                console.log(topicData.topic.slug);
-                return topicData;
-            }
-        }
-    };
-
-})(module.exports);
+const plugin = {};
+plugin.topicCreate = (topicData) => {
+    topicData.topic.slug = `${topicData.topic.tid}/topic`;
+    console.log(topicData.topic.slug);
+    return topicData;
+}
+plugin.topicEdit = (topicData) => {
+    topicData.topic.slug = `${topicData.topic.tid}/topic`;
+    console.log(topicData.topic.slug);
+    return topicData;
+}
+module.exports = plugin;

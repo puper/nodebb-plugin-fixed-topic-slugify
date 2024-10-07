0dfdd57f41696ba209646522f0991c0f8f1fe91a
Pu Pu <master@131411.xyz>
2024-10-07 14:11:11 +0800

    up

--- plugin.js
+++ plugin.js
@@ -3,38 +3,17 @@
  */
 (function (Plugin) {
     'use strict';
-
-    var translit = require('translitit-cyrillic-russian-to-latin');
-
     //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
     Plugin.hooks = {
         filters: {
-            categoryCreate: function (categoryData, callback) {
-                categoryData.category.slug = translit(categoryData.category.slug);
-                callback(null, categoryData);
-            },
-
-            categoryUpdate: function (categoryData, callback) {
-                if ('slug' in categoryData.category) {
-                    categoryData.category.slug = translit(categoryData.category.slug);
-                }
-                callback(null, categoryData);
-            },
-
             topicCreate: function (topicData, callback) {
-                topicData.topic.slug = translit(topicData.topic.slug);
+                topicData.topic.slug = `${topicData.tid}/topic`;
                 callback(null, topicData);
             },
 
             topicEdit: function (topicData, callback) {
-                topicData.slug = translit(topicData.slug);
+                topicData.slug = `${topicData.tid}/topic`;
                 callback(null, topicData);
-            },
-
-            userCreate: function (userData, callback) {
-                //If there will be username collision, userslug will be overridden by NodeBB...
-                userData.userslug = translit(userData.userslug);
-                callback(null, userData);
             }
         }
     };
